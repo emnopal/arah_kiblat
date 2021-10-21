60cd4f5d4efddfaf72837cc796be695481a645f5
munaufal <mnaufal0999@gmail.com>
2021-10-21 22:53:27 +0700

    Commit Pertama

---
+++ generate_arah/models.py
@@ -0,0 +1,17 @@
+import requests
+
+from django.db import models
+
+# Create your models here.
+class Parse(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    lokasi = models.CharField(max_length=1000)
+    sudut = models.CharField(max_length=1000)
+
+    class Meta:
+        ordering = ["-created"]
+
+    def save(self, *args, **kwargs):
+        r = requests.get(f"http://127.0.0.1:8000/{self.lokasi}")
+        self.sudut = r.json()
+        super().save(*args, **kwargs)
