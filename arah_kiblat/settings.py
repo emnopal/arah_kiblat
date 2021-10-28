3c78cf9b4ee068c1322b157a9bd0c7c2830087da
Muhammad Naufal <mnaufal0999@gmail.com>
2021-10-28 22:47:42 +0700

    fixing something

--- arah_kiblat/settings.py
+++ arah_kiblat/settings.py
@@ -17,7 +17,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
