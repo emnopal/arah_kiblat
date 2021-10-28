cd01a284fa85d05c12d382506adfeec8e4d87bdb
Muhammad Naufal <mnaufal0999@gmail.com>
2021-10-28 21:44:55 +0700

    fixing something

--- generate_arah/models.py
+++ generate_arah/models.py
@@ -12,7 +12,7 @@ class Parse(models.Model):
         ordering = ["-created"]
 
     def save(self, *args, **kwargs):
-        #r = requests.get(f"https://arah-kiblat-api.herokuapp.com/{self.lokasi}")
-        r = requests.get(f"http://127.0.0.1:8000/{self.lokasi}")
+        r = requests.get(f"https://arah-kiblat-api.herokuapp.com/{self.lokasi}")
+        #r = requests.get(f"http://127.0.0.1:8000/{self.lokasi}")
         self.sudut = r.json()
         super().save(*args, **kwargs)
