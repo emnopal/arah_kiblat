397ff9d13c28d48c36a23a6783dcd5ff76cbf4d8
Muhammad Naufal <mnaufal0999@gmail.com>
2021-11-21 08:39:12 +0700

    fixed layout for mobile devices and added copyright footer

--- arah_kiblat/settings.py
+++ arah_kiblat/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
