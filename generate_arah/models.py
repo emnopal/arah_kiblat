cdb7c08c30104f61de9a207b3c61bae4dd5dfe39
Muhammad Naufal <mnaufal0999@gmail.com>
2021-12-11 14:20:03 +0700

    added .env

--- generate_arah/models.py
+++ generate_arah/models.py
@@ -12,7 +12,6 @@ class Parse(models.Model):
         ordering = ["-created"]
 
     def save(self, *args, **kwargs):
-        #r = requests.get(f"https://arah-kiblat-api.herokuapp.com/{self.lokasi}")
-        r = requests.get(f"http://127.0.0.1:5500/{self.lokasi}")
+        r = requests.get(f"https://arah-kiblat-api.herokuapp.com/{self.lokasi}")
         self.sudut = r.json()
         super().save(*args, **kwargs)
